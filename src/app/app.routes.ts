942541b41e913ad4447bb49c26573fa38b48f941
gabrielpereira1603 <pereiragabrieldev@gmail,com>
2024-05-24 15:45:32 -0300

    feat: add fallback router and page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SingupComponent } from './pages/singup/singup.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeClienteComponent } from './components/home-cliente/home-cliente.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
   {
     path: "login",
     component: LoginComponent
@@ -10,5 +17,14 @@ export const routes: Routes = [
   {
     path: "criarconta",
     component: SingupComponent
+  },
+  {
+    path: "clienthome",
+    component: HomeClienteComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
+
 ];
